4c9b39bd9432cd6390d292d0b470a63688aa5360
cerna234 <cerna2424@gmail.com>
2023-02-13 11:36:51 -0800

    about Section v1

---
+++ my-app/components/AboutSection/ImageComponent.js
@@ -0,0 +1,24 @@
+import Styles from "../AboutSection/about.module.scss"
+import Image from "next/image";
+import Img1 from "../../public/Images/aboutImg.png"
+import Img2 from "../../public/Images/aboutImg2.png"
+const ImageComponent = () => {
+    return(
+        <div className={Styles.aboutSectionContainer}>
+            <Image
+                src={Img1}
+                alt="img"
+                className={Styles.aboutImageOne}
+            />
+
+            <Image
+                src={Img2}
+                alt="Img"
+                className={Styles.aboutImageTwo}
+            />
+        </div>
+    )
+}
+
+
+export default ImageComponent;
---
+++ my-app/components/AboutSection/aboutInfo.js
@@ -0,0 +1,19 @@
+import Styles from "../AboutSection/about.module.scss"
+const AboutInfo = () => {
+    return(
+        <div className={Styles.aboutSectionContainer}>
+            <div>
+               <h2 className={Styles.aboutTitle}>ABOUT TONE ELECTRIC</h2>
+            </div>
+            <div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a </p>
+            </div>
+            <div>
+            LEARN MORE ABOUT  OUR TEAM
+            </div>
+        </div>
+    )
+}
+
+
+export default AboutInfo;
