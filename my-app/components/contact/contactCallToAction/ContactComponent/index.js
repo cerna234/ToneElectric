08aa4b49f68c7180a1388a4db9c9f28adcea8cfe
cerna234 <cerna2424@gmail.com>
2023-02-18 07:30:05 -0800

    update

---
+++ my-app/components/contact/contactCallToAction/ContactComponent/index.js
@@ -0,0 +1,11 @@
+import Styles from "../ContactComponent/contact.module.scss"
+
+const ContactComponent = () => {
+    return(
+        <div className={Styles.contactContainer}>
+            <p>CONTACT</p>
+        </div>
+    )
+}
+
+export default ContactComponent;
