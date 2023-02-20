7438f5b7b6122cd1d82af1d171b66e07af14e8c7
cerna234 <cerna2424@gmail.com>
2023-02-19 22:23:25 -0800

    footer v1

--- my-app/components/Footer/index.js
+++ my-app/components/Footer/index.js
@@ -1,8 +1,64 @@
 import Styles from "../Footer/footer.module.scss"
+import Image from "next/image";
+import Logo from "../../public/Images/logo.png"
 const Footer = () => {
     return(
         <div className={Styles.footerContainer}>
-            <p>FOOTER</p>
+            <div className={Styles.footerTopHalf}>
+                <div className={Styles.footerSection}>
+                    <div className={Styles.footerSectionInner}>
+                        <div className={Styles.titleSection}>
+                            <Image
+                                src={Logo}
+                                height={100}
+                                width={130}
+                                className={Styles.footerLogo}
+                                alt="logo of Tone Electric"
+                            /> 
+                        </div>
+                        
+                        <div className={Styles.info}>
+                            <p className={Styles.logoSectionText}>TONE ELECTRIC</p>
+                            <p className={Styles.logoSectionText}>since 1988</p>
+                        </div>
+                    </div>
+                    
+                    
+
+                </div>
+                <div className={Styles.footerSection}>
+                    <div className={Styles.footerSectionInner}>
+                        <div className={Styles.titleSection}>
+                            <h2>OFFICE</h2>
+                        </div>
+                        <div className={Styles.info}>
+                            <p>Tone Electric
+                                20793 E Valley Blvd Unit B
+                                Walnut, CA 91789
+                            </p>
+                            <p>Fax: (909) 594-8460</p>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className={Styles.footerSection}>
+                    <div className={Styles.footerSectionInner}>
+                        <div className={Styles.titleSection}>
+                            <h2>CONTACT</h2>
+                        </div>
+                        <div className={Styles.info}>
+                            <p>TEL: (909) 594-8460</p>
+                            <p>Email: info@tonelectric.com</p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div className={Styles.footerBottomhalf}>
+                <p>COPYRIGHT@COPYRIGHT</p>
+            </div>
+            
         </div>
     )
 }
