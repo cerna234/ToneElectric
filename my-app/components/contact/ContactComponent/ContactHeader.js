d89cc24161f2e24fe4ecc80967bae582c2d4e0c1
cerna234 <cerna2424@gmail.com>
2023-02-23 16:30:54 -0800

    Responsiveness version 1

--- my-app/components/contact/ContactComponent/ContactHeader.js
+++ my-app/components/contact/ContactComponent/ContactHeader.js
@@ -3,11 +3,14 @@ import Styles from "../ContactComponent/contact.module.scss"
 const ContactHeader = () => {
     return(
         <div className={Styles.contactHeaderComponent}>
-            <div className={Styles.headerInfo}>
-                <h2>WELCOME TO TONE ELECTRIC</h2>
-                <p>Interested?</p>
-                <p>Fill the form and we&apos;ll be in touch</p>
+            <div className={Styles.contactHeaderOverlay}>
+                <div className={Styles.headerInfo}>
+                    <h2>WELCOME TO TONE ELECTRIC</h2>
+                    <p>Interested?</p>
+                    <p>Fill the form and we&apos;ll be in touch</p>
+                </div>
             </div>
+           
 
         </div>
     )
