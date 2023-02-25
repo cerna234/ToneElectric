5fbb5f1caae749767a4dae872fec0af46290633c
cerna234 <cerna2424@gmail.com>
2023-02-25 07:47:47 -0800

    responsive v2

--- my-app/components/AboutSection/ImageComponent.js
+++ my-app/components/AboutSection/ImageComponent.js
@@ -1,14 +1,15 @@
 import Styles from "../AboutSection/about.module.scss"
 import Image from "next/image";
-import Img1 from "../../public/Images/amzFresh.png"
 import Img2 from "../../public/Images/aboutImg.png"
 const ImageComponent = () => {
     return(
-        <div className={Styles.aboutSectionContainer}>
+        <div  className={Styles.aboutSectionImageContainer}>
            <Image 
             src={Img2}
             className={Styles.aboutImage}
             alt="building image"
+            height={200}
+            width={200}
            />
         </div>
     )
