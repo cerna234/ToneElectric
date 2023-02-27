fa274c56e23a7ed65f092d94f9ea417b6e67d9ee
cerna234 <cerna2424@gmail.com>
2023-02-27 10:47:02 -0800

    added bootstrap v5 and developed version 2 of navBar

--- my-app/components/nav/index.js
+++ my-app/components/nav/index.js
@@ -1,30 +1,39 @@
 import * as React from "react";
 import Link from "next/link";
-import NavStyles from "../nav/nav.module.scss"
+import Styles from "../nav/nav.module.scss"
 import Image from "next/image";
 import Logo from "../../public/Images/logo.png"
 
 
-const Nav = () => {
+
+const NavBar = () => {
     return (
-    <nav className={NavStyles.navContainer}>
-      <div>
-          <Image
-          src={Logo}
-          alt="logo"
-          width={100}
-          heigh={200}
-          />
-      </div>
-      <div>
-      <Link href="/">HOME</Link>
-       <Link href="/contact">CONTACT</Link>
-       <Link href="/projects">PROJECTS</Link>
-      </div>
-     
-    </nav>
+      <nav className=  {`${Styles.navOverride} navbar navbar-expand-lg navbar-light bg-light `}>
+  <a class="navbar-brand" href="#">LOGO</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ms-auto">
+      
+      <li className={`nav-item`}>
+        <Link className={`${Styles.links} nav-link`} href="/">Home</Link>
+      </li>
+      
+      <li className={`nav-item`}>
+        <Link className={`${Styles.links} nav-link`} href="/contact">Contact</Link>
+      </li>
+
+      <li className={`nav-item`}>
+        <Link className={`${Styles.links} nav-link`} href="/">Projects</Link>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
 
       );
     };
 
-export default Nav;
+export default NavBar;
--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,8 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import '@/styles/globals.css'
 import Nav from "../components/nav/index"
 import Footer from '@/components/Footer'
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+
+  
+
+
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap');
+  }, []);
+  
   return (
 
     <>
